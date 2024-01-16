6fe766e3efd61f5f387453a5a8e17269f0fb6867
Viper <106883384+Rudal-XD@users.noreply.github.com>
2024-01-16 08:09:29 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -220,6 +220,22 @@ def encryptvar():
     print(f"{out_file} saved in {pwd}")
     mover(out_file)
 
+# Base64 encoder function
+def obfuscate(VARIABLE_NAME, file_content):
+    b64_content = base64.b64encode(file_content.encode()).decode()
+    index = 0
+    code = f'{VARIABLE_NAME} = ""\n'
+    for _ in range(int(len(b64_content) / OFFSET) + 1):
+        _str = ''
+        for char in b64_content[index:index + OFFSET]:
+            byte = str(hex(ord(char)))[2:]
+            if len(byte) < 2:
+                byte = '0' + byte
+            _str += '\\x' + str(byte)
+        code += f'{VARIABLE_NAME} += "{_str}"\n'
+        index += OFFSET
+    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
+    return code
 
 # Decrypt bash code by "eval"
 def decryptsh():
